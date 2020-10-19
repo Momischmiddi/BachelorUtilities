1a143aff41da523903cea4e497f79758d5b2f5f0
Moritz <34316151+Momischmiddi@users.noreply.github.com>
2020-10-19 22:30:05 +0200

    Update main.java
    Removed dead comments and reformatted code due to SonarQube

--- SysAdmin/src/main/java/main/main.java
+++ SysAdmin/src/main/java/main/main.java
@@ -17,7 +17,6 @@ import backend.database.dbQueries.SearchQueries;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DBCredentials credentials = new DBCredentials();
 		credentials.setHostAdress("localhost");
 		credentials.setPort(3306);
@@ -29,48 +28,9 @@ public class main {
 		try {
 			DBConnection connection = openConnection.createConnection(credentials);
 			InsertQueries insert = new InsertQueries(connection);
-//			for(int i=0;i<20;i++){
-//				Topic topic = new Topic();
-//				topic.setTitle("Topic " + i);
-//				topic.setDescription("Description for Topic " + i);
-//				
-//				Author author = new Author();
-//				author.setForename("Sebastian " + i);
-//				author.setName("Lohr" + i);
-//				author.setMatriculationNumber(26044);
-//				
-//				ExpertOpinion expertOpinion = new ExpertOpinion();
-//				expertOpinion.setForename("Tobias" + i);
-//				expertOpinion.setName("Eggendorfer" + i);
-//				expertOpinion.setOpinion("ganz ok ..." + i);
-//				
-//				SecondOpinion secondOpinion = new SecondOpinion();
-//				secondOpinion.setForename("Thorsten" + i);
-//				secondOpinion.setName("Weiss" + i);
-//				secondOpinion.setOpinion("find ich gut!" + i);
-//				
-//				Grade grade = new Grade();
-//				grade.setGrade(1+(i)/10);
-//				
-//				topic.setAuthor(author);
-//				topic.setExpertOpinion(expertOpinion);
-//				topic.setSecondOpinion(secondOpinion);
-//				topic.setGrade(grade);
-//				insert.insertNewTopic(topic);
-//			}
-//			for(int i = 0; i<20;i++){
-//				for(int j =0; j<20; j++){
-//					Date date = new Date();
-//					date.setDate("2016-10-" + j);
-//					date.setName("Meeting nr. " + j);
-//					insert.executeInsertStatement(date, i+1);
-//				}
-//			}
-			
 			SearchQueries search = new SearchQueries(connection);
 			ArrayList<Topic> topics = search.searchAllTopics();
 			Topic topic = search.getSpecificTopic(3);
-			
 			connection.closeConnection();
 		} catch (Exception e) {
 			System.err.println("Error establishing connection");
